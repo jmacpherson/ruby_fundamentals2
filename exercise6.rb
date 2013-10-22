02473c0b9344564f2f355f8afcf42c97fe88e312
jmacpherson <jd.macpherson@gmail.com>
2013-10-22 14:19:50 -0400

    made list, wrote method to output list, added items, checked how many items, checked to see if bananas were on list

---
+++ exercise6.rb
@@ -0,0 +1,22 @@
+grocery_list = ["apples","toilet paper","chocolate","salmon"]
+
+def print_list(list)
+  list.each { |item| puts "*\t#{item}" }
+end
+
+print_list(grocery_list)
+
+#add item
+grocery_list << "rice"
+
+print_list(grocery_list)
+
+#how many items?
+puts grocery_list.length
+
+#do you need to pickup bananas?
+if grocery_list.include?("bananas")
+	puts "You need to pick up bananas."
+else
+	puts "You don't need to pick up bananas today."
+end
