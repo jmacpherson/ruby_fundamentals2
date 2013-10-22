3cc46239f9bd3b6f43bb8fcbbfb157d365a16c45
jmacpherson <jd.macpherson@gmail.com>
2013-10-22 15:13:10 -0400

    exercise7.rb - wrote method to display cohorts' enrollment

--- exercise7.rb
+++ exercise7.rb
@@ -3,3 +3,11 @@ students = {
   :cohort2 => 42,
   :cohort3 => 22
 }
+
+def show_cohort_enrollment(cohort_list)
+  cohort_list.each do |key, value|
+    puts "#{key.to_s}: #{value} students"
+  end
+end
+
+show_cohort_enrollment(students)
